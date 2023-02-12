994d2683a871d477fab74af0c21c2a45618f4b06
Алексей Немирович <roooly@yandex.ru>
2023-02-12 22:47:51 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
-    print(start_training(char_name, char_class)) skip
+    print(start_training(char_name, char_class))
 
 
 main()
